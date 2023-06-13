fe2373311f6eb13514e1251b99eb7e3c53bf0313
aghlaoui <105565841+aghlaoui@users.noreply.github.com>
2023-06-13 15:46:05 +0100

    edits

--- wp-content/themes/cooperate/assets/splited-code/js/contact.js
+++ wp-content/themes/cooperate/assets/splited-code/js/contact.js
@@ -1,22 +1,24 @@
 
+(function ($) {
+    "use strict";
+    $(document).ready(function () {
+        $('.screen-reader-response').hide();
+        $(document).on('scroll', function (event) {
+            // Get the query parameters from the URL
+            var params = new URLSearchParams(window.location.search);
 
-$(document).ready(function () {
-    $('.screen-reader-response').hide();
-    $(document).on('scroll', function (event) {
-        // Get the query parameters from the URL
-        var params = new URLSearchParams(window.location.search);
+            var plan = params.get('plan');
+            var type = params.get('type')
 
-        var plan = params.get('plan');
-        var type = params.get('type')
+            if (plan != null && type != null) {
+                $('#contact-subject').val('About : ' + plan + ' & ' + type);
+            }
+        });
 
-        if (plan != null && type != null) {
-            $('#contact-subject').val('About : ' + plan + ' & ' + type);
-        }
-    });
+        $('.wpcf7-response-output').hide();
 
-    $('.wpcf7-response-output').hide();
-
-    $('.submit-green1').on('click', function () {
-        $('.wpcf7-response-output').show();
+        $('.submit-green1').on('click', function () {
+            $('.wpcf7-response-output').show();
+        })
     })
-})
+})(jQuery);
